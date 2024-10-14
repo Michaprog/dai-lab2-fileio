7bb723c65bdde7af709c5d6c94d38bb016b058ab
Michaprog <vejnstejn2@gmail.com>
2024-10-14 16:20:38 +0200

    Created FileReaderWriter.java for Michaprog

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Michaprog/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Michaprog/FileReaderWriter.java
@@ -14,7 +14,29 @@ public class FileReaderWriter {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        return null;
+
+        if (file == null || encoding == null) {
+            return null;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        try (
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(
+                                new FileInputStream(file),
+                                encoding))
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+
+        }
+        return result.toString();
     }
 
     /**
@@ -28,6 +50,23 @@ public class FileReaderWriter {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+
+        if (file == null || content == null|| encoding == null) {
+            return false;
+        }
+
+        try (
+                FileOutputStream fos = new FileOutputStream(file);
+                OutputStreamWriter osw = new OutputStreamWriter(fos, encoding);
+                BufferedWriter bw = new BufferedWriter(osw)
+        ) {
+            bw.write(content);
+            bw.flush();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+
+        }
     }
 }
